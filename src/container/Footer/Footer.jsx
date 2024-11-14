b5aa0432cd22ee020f7fe790ac7dd8ad5ef392a1
KibromHS <kibromhs81@gmail.com>
2024-11-14 20:42:43 +0300

    footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,110 +1,111 @@
-import React from 'react';
+import React from "react";
+import { footerBg, logo, underline } from "../../img";
+import {
+  FaChevronRight,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaMailBulk,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer class="footer-bg footer-p">
-     
-    <div class="overly"><img src="https://libratechnologies.net/stgcarehome/wp-content/themes/mecare/img/an-bg/footer-bg.png" alt="rest" /></div>
-        <div class="footer-top pb-30" style="background-color: #ECF1FA;">
-        <div class="container">
-            <div class="row justify-content-between">
-                
-                <div class="col-xl-3 col-lg-3 col-sm-6">
-                    <div class="footer-widget mb-30">
-                        <div class="flog mb-35">
-                            <a href="https://libratechnologies.net/stgcarehome/">
-                                                            <img src="https://libratechnologies.net/stgcarehome/wp-content/uploads/2024/10/logo.png" alt="logo" />
-                                                        </a>
-                        </div>
-                                                <div class="footer-social">
-                               
-                            <a href="#" title="Facebook"><i class="fab fa-facebook"></i></a>
-                                                           
-                            <a href="#" title="Twitter"><i class="fab fa-twitter"></i></a>
-                                                           
-                            <a href="#" title="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
-                             
-                               
-                            <a href="#" title="Instagram"><i class="fab fa-instagram"></i></a>
-                                   
-                        </div>
-                    </div>
-                </div>
-                
-                
-                <div class="col-xl-2 col-lg-2 col-sm-6">
-                    <div class="footer-widget mb-30">
-                        
-                         <div class="f-widget-title"><h5> Our Links </h5></div><div class="textwidget custom-html-widget"><div class="footer-link">
-    <ul>                                         <li><a href="#"><i class="fas fa-chevron-right"></i> Partners</a></li>
-		<li><a href="#"><i class="fas fa-chevron-right"></i> About Us</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Career</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Reviews</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Terms &amp; Conditions</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Help</a></li>
-    </ul>
-</div></div> 
-                                            </div>
-                </div>
-                <div class="col-xl-2 col-lg-2 col-sm-6">
-                    <div class="footer-widget mb-30">
-                        
-                          <div class="f-widget-title">
-                            <h5>Other Links </h5></div><div class="textwidget custom-html-widget"><div class="footer-link">
-    <ul>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Home</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> About Us</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Services</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Project</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Our Team</a></li>
-        <li><a href="#"><i class="fas fa-chevron-right"></i> Latest Blog</a></li>
-    </ul>
-</div></div> 
-                                            </div>
-                </div>  
-                <div class="col-xl-3 col-lg-3 col-sm-6">
-                    <div class="footer-widget mb-30">
-                        
-                         <div class="f-widget-title">
-                            <h5> Contact Us </h5></div><div class="textwidget custom-html-widget"><div class="footer-link">
-    <div class="f-contact">
-        <ul>
-            <li>
-                <i class="icon dripicons-phone"></i>
-                <span>+251911000000<br />+251911000000</span>
-            </li>
-            <li>
-                <i class="icon dripicons-mail"></i>
-                 <span><a href="mailto:info@stgabrieladultcare.com">info@stgabrieladultcare.com</a><br /><a href="mailto:contact@stgabrieladultcare.com">contact@stgabrieladultcare.com</a></span>
-            </li>
-            <li>
-              <i class="fal fa-map-marker-alt"></i>
-                 <span>Addis Ababa <br />Ethiopia</span>
-            </li>
-        </ul>
-    </div>
-</div></div> 
-                                            </div>
-                </div>
-                
-            </div>
+    <footer>
+      <img src={footerBg} alt="background" className="footer-bg" />
+      <div className="footer-main">
+        <div className="social-links">
+          <img src={logo} alt="" />
+          <p>
+            Our dedicated RN-certified staff ensures <br />
+            personalized attention and support for each
+            <br /> resident, helping them maintain dignity,
+            <br />
+            independence, and quality of life{" "}
+          </p>
+          <div className="social">
+            <a href="https://facebook.com" target="_blank">
+              <FaFacebook />
+            </a>
+            <a href="https://twitter.com" target="_blank">
+              <FaTwitter />
+            </a>
+            <a href="https://linkedin.com" target="_blank">
+              <FaLinkedin />
+            </a>
+            <a href="https://instagram.com" target="_blank">
+              <FaInstagram />
+            </a>
+          </div>
         </div>
-    </div>
-    <div class="copyright-wrap">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="copyright-text text-center">
-                        
-                        <p>© 2024 St Gabriel Adult Care Home</p>
-
-                                            </div>
-                </div>
-            </div>
+        <div className="home-links">
+          <h2>Our Links</h2>
+          <img src={underline} alt="" />
+          <ul>
+            <li>
+              <a href="#home">
+                <FaChevronRight />
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about">
+                <FaChevronRight />
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#services">
+                <FaChevronRight />
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#contact">
+                <FaChevronRight />
+                Contact Us
+              </a>
+            </li>
+          </ul>
         </div>
-    </div>
-</footer>
-  )
-}
+        <div className="contact-us">
+          <h2>Contact Us</h2>
+          <img src={underline} alt="" />
+          <ul>
+            <li>
+              <a href="tel:+251911000000">
+                <FaPhone />
+                +251911000000
+              </a>
+            </li>
+            <li>
+              <a href="mailto:info@stgabrieladultcare.com">
+                <FaMailBulk />
+                info@stgabrieladultcare.com
+              </a>
+            </li>
+            <li>
+              <a href="https://google.com/maps">
+                <FaLocationDot />
+                Addis Ababa, Ethiopia
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="copyright">
+        <p>
+          &copy; 2024 St Gabriel Nursing Home | Powered by{" "}
+          <a href="https://cepheusx.com" target="_blank">
+            CepheusX
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { About, Contact, Main, Navbar, Our } from '../../container'
+import React from "react";
+import { About, Contact, Footer, Main, Navbar, Our } from "../../container";
 
 const Home = () => {
   return (
     <>
-        <Navbar />
-        <Main />
-        <About />
-        <Our />
-        <Contact />
+      <Navbar />
+      <Main />
+      <About />
+      <Our />
+      <Contact />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
