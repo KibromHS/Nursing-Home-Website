73d7a44fed01a1b304d53078b645a1839eb84418
KibromHS <kibromhs81@gmail.com>
2024-12-06 00:31:44 +0300

    add testimonials

--- src/container/Testimonial/Mskr.jsx
+++ src/container/Testimonial/Mskr.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { quote } from "../../img";
 import "./Mskr.css";
 
-const Mskr = ({ message, img, name, profession, style }) => {
+const Mskr = ({ message, name, profession, style }) => {
   return (
     <div className="mskr" style={style}>
       <p className="message">{message}</p>
       <img src={quote} alt="quote" className="quote" />
       <div className="person">
-        <img src={img} alt="person" className="profile" />
+        <div className="profile">{name[0]}</div>
         <div className="profile-desc">
           <h3>{name}</h3>
           <p>{profession}</p>
--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -15,24 +15,21 @@ const Testimonial = () => {
   const testimonials = [
     {
       message:
-        "They took care of my father in his last few weeks of life. They made his last few days as comfortable as they could. They are the best hands down! They treated him with respect and kindness every minute. They are the best care givers I have ever had the pleasure of knowing.",
-      profile: profile1,
-      name: "Barry Phraes",
-      profession: "Local Guide",
+        "My grandmother was cared for by Mimi and her staff for the last couple years leading up to her death. The owners and staff at Saint Gabriel are wonderful. The love they give to their patients is like no other we’ve seen. My grandmothers life even after her death was so cherished by Mimi and her family. Working with Dementia patients is such a difficult job and Mimi and her staff were so good at it. I would have no hesitation having your loved one live her.",
+      name: "Lindsey Myers",
+      profession: "Customer",
     },
     {
       message:
-        "They are the best hands down! They treated him with respect and kindness every minute. They are the best care givers I have ever had the pleasure of knowing.",
-      profile: profile1,
-      name: "Kibrom",
-      profession: "Local Guide",
+        'I am so very grateful to have my client in residence at St. Gabriel AFH.  An aging person suffering in the throws of dementia requires a safe, consistent, warm, engaging environment in addition to quality care and nutritious food.  My client is well cared for and is happy in what he calls his "home".  Thank you Mimi and your amazing staff.',
+      name: "Coya Eubank-Kirby",
+      profession: "Customer",
     },
     {
       message:
-        "Moving my mother to Saint Gabriel was the best decision our family made. The staff is incredibly caring, and Mom is thriving in this wonderful environment.",
-      profile: profile1,
-      name: "Jane D.",
-      profession: "Daughter of Resident",
+        "Mimi is an incredible, professional and caring caregiver who maintains high standards.  She thoroughly trains her staff and has high expectations for them as well.  Her attention to detail is without fault and she cares deeply for any in her care and welcomes all to her AFH.",
+      name: "Maria Othman",
+      profession: "Customer",
     },
   ];
 
@@ -87,7 +84,6 @@ const Testimonial = () => {
             <Mskr
               key={index}
               message={testimonial.message}
-              img={testimonial.profile}
               name={testimonial.name}
               profession={testimonial.profession}
               style={itemStyle}
